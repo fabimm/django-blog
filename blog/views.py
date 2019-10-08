2932f93f64854b033ffa4a5b4a8f1bce26996643
Fabiola <00015313@uca.edu.sv>
2019-10-08 07:48:04 -0600

    Pruebas con bootstrap, sin funcionalidad.

--- blog/views.py
+++ blog/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
+
+#def index(request):
+    #return render(request, 'index.html')
